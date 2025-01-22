269ba010a357caaa0c68c905c45ad8a4f345bdeb
madicode <mharri48@terpmail.umd.edu>
2025-01-22 18:12:54 -0500

    completed interest page (no font added yet)

--- eventTerp/App.js
+++ eventTerp/App.js
@@ -9,9 +9,18 @@ import LandingScreen from "../eventTerp/screens/Landing.js";
 import AddEvent from "../eventTerp/screens/AddEvent.js";
 import Swipes from "../eventTerp/screens/Swipes.js";
 import Calendar from "../eventTerp/screens/Calendar.js";
-
+import Interests from "../eventTerp/screens/Interests.js";
 
 const Tab = createBottomTabNavigator();
+
+const Tabs =() => {
+  return
+  <Tab.Navigator
+    tabBarOptions={{
+      tabBarStyle: { backgroundColor: 'gold' },
+  }}
+></Tab.Navigator>
+}
 
 export default function App() {
   return(
@@ -27,12 +36,20 @@ export default function App() {
 
 
         <Tab.Screen 
-        name='Add Event' 
-        component={AddEvent}
-        options={{
-          tabBarIcon: ({focused}) => <AntDesign name='pluscircleo' size='20' color='#ffd200' />,
-        }}
-        />
+          name='Add Event' 
+          component={AddEvent}
+          options={{
+            tabBarIcon: ({focused}) => 
+            <AntDesign name='plus' size='20' color='#ffd200'  />,
+          }}/>
+        <Tab.Screen 
+          name='Interests' 
+          component={Interests}
+          options={{
+            tabBarIcon: ({focused}) => 
+            <AntDesign name='hearto' size='20' color='#ffd200'  />,
+          }}/>
+
         <Tab.Screen 
         name='Swipes' 
         component={Swipes}
@@ -40,6 +57,9 @@ export default function App() {
           tabBarIcon: ({focused}) => <AntDesign name='arrowright' size='20' color='#ffd200' />,
         }}
         />
+
+
+
         <Tab.Screen 
         name='Calendar' 
         component={Calendar}
---
+++ eventTerp/screens/Interests.js
@@ -0,0 +1,148 @@
+import { View, Text, StyleSheet, Button, TouchableOpacity } from 'react-native'
+import React, { useState } from 'react';
+
+
+
+export default function Interests() {
+  const [interests, setInterests]=useState([]);
+
+  
+  const saveInterest = (title) => {
+    if (!interests.includes(title)) {
+      setInterests((prevInts) => {
+        const updatedInterests = [...prevInts, title];
+        console.log(updatedInterests); 
+        return updatedInterests; 
+      });
+    }
+    if (interests.includes(title)){
+      setInterests((prevInts) => prevInts.filter((interests) => interests !== title )
+    )
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      {/* Render interests if the list is not empty */}
+      {interests.length > 0 && (
+        <View style={styles.intlist}>
+          {interests.map((interest, index) => (
+            <View key={index} style={styles.intItem}>
+              <Text style={styles.inttext}>{interest}</Text>
+            </View>
+          ))}
+        </View>
+      )}
+      
+      <Text style={styles.heading}>What Are Your Interests?</Text>
+
+      
+      <TouchableOpacity
+        style={[styles.button, { position:'absolute', top: 200, right:270 }]}  onPress={() => saveInterest('Academic')}>
+        <Text style={styles.buttontext}> Academic </Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={[styles.button, { position:'absolute', top: 200, right:150 }]} onPress={() => saveInterest('Career Development')}>
+        <Text style={styles.buttontext}>Career Development</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={[styles.button, { position:'absolute', top: 200, right:30 }]} onPress={() => saveInterest('Competition')}>
+        <Text style={styles.buttontext}>Competition</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={[styles.button, { position:'absolute', top: 295, right:150 }]} onPress={() => saveInterest('Cultural')}>
+        <Text style={styles.buttontext}>Cultural</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={[styles.button, { position:'absolute', top: 295, right:270 }]} onPress={() => saveInterest('Educational')}>
+        <Text style={styles.buttontext}>Educational</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={[styles.button, { position:'absolute', top: 390, right:150 }]} onPress={() => saveInterest('Entertainment')}>
+        <Text style={styles.buttontext}>Entertainment</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={[styles.button, { position:'absolute', top: 390, right:30 }]} onPress={() => saveInterest('Health/Wellness')}>
+        <Text style={styles.buttontext}>Health/Wellness</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={[styles.button, { position:'absolute', top: 390, right:270 }]} onPress={() => saveInterest('Lecture/Speaker')}>
+        <Text style={styles.buttontext}>Lecture/Speaker</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={[styles.button, { position:'absolute', top: 485, right:270 }]} onPress={() => saveInterest('Music')}>
+        <Text style={styles.buttontext}>Music</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={[styles.button, { position:'absolute', top: 485, right:150 }]} onPress={() => saveInterest('Networking')}>
+        <Text style={styles.buttontext}>Networking</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={[styles.button, { position:'absolute', top: 295, right:30 }]} onPress={() => saveInterest('Off-Campus')}>
+        <Text style={styles.buttontext}>Off-Campus</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={[styles.button, { position:'absolute', top: 485, right:30 }]} onPress={() => saveInterest('Religious')}>
+        <Text style={styles.buttontext}>Religious</Text>
+      </TouchableOpacity>
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+    alignItems: 'center',
+    width:400
+  },
+  intlist:{
+    flexDirection: 'row',
+    flexWrap:'wrap',
+    left:0,
+    position:'absolute',
+    top:90,
+    backgroundColor:'rgba(300, 0, 0, 0.5)',
+    opacity:10,
+    borderRadius:90,
+  },
+  inttext:{
+    fontSize:15,
+    position:'center',
+    margin:5,
+    position:'relative',
+    left:15
+
+  },
+  heading:{
+    fontSize:30,
+    fontWeight:'bold',
+    backgroundColor:'red',
+    top:'30',
+    position:'absolute'
+},
+button:{
+  backgroundColor:'gold',
+  width:100,
+  height:50,
+  borderRadius:50,
+},
+
+buttontext:{
+  textAlign:'center',
+  fontWeight:'bold',
+  position:'relative',
+  top:15,
+},
+})
